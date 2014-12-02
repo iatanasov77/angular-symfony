0d21304ff3bca011942b567e7ffdd26ca2529c67
Ivan I. Atanasov <iatanasov.icygen.com>
2014-12-02 17:45:55 +0200

    IAAngularApplication GenerateBundle Command

---
+++ src/IA/Bundle/AngularApplicationBundle/Command/GenerateBundleCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace IA\Bundle\AngularApplicationBundle\Command;
+
+use IA\Bundle\AngularApplicationBundle\Generator\BundleGenerator;
+use Sensio\Bundle\GeneratorBundle\Command\GenerateBundleCommand as SensioGenerateBundleCommand;
+
+/**
+ * Generates Angular Application bundles.
+ *
+ * @author Ivan I. Atanasov <iatanasov77@gmail.com>
+ */
+class GenerateBundleCommand extends SensioGenerateBundleCommand
+{
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        parent::configure();
+        $this->setName('ia:generate:angularbundle');
+    }
+
+    protected function createGenerator()
+    {
+        return new BundleGenerator($this->getContainer()->get('filesystem'));
+    }
+}
---
+++ src/IA/Bundle/AngularApplicationBundle/Generator/BundleGenerator.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace IA\Bundle\AngularApplicationBundle\Generator;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Sensio\Bundle\GeneratorBundle\Generator\Generator;
+use Sensio\Bundle\GeneratorBundle\Generator\BundleGenerator as SensioBundleGenerator;
+
+/**
+ * Generates a bundle.
+ *
+ * @author Ivan I. Atanasov <iatanasov77@gmail.com>
+ */
+class BundleGenerator extends Generator
+{
+    private $_filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->_filesystem = $filesystem;
+    }
+
+    public function generate($namespace, $bundle, $dir, $format, $structure)
+    {
+        $sensioGenerator = new SensioBundleGenerator($this->_filesystem);
+        $sensioGenerator->generate($namespace, $bundle, $dir, $format, $structure);
+        
+        /*
+         *  Here is my code for generation
+         */
+        $dir .= '/'.strtr($namespace, '\\', '/');
+        $basename = substr($bundle, 0, -6);
+        $parameters = array(
+            'namespace' => $namespace,
+            'bundle'    => $bundle,
+            'format'    => $format,
+            'bundle_basename' => $basename,
+            'extension_alias' => Container::underscore($basename),
+        );
+        
+        $this->renderFile('bundle/Extension.php.twig', $dir.'/DependencyInjection/'.$basename.'Extension.php', $parameters);
+        $this->renderFile('bundle/requirejs.yml.twig', $dir.'/Resources/config/requirejs.yml', $parameters);
+        $this->renderFile('bundle/routing_angular.yml.twig', $dir.'/Resources/config/routing_angular.yml', $parameters);
+        $this->renderFile('bundle/bower.json.twig', $dir.'/Resources/config/bower.json', $parameters);
+    }
+}
