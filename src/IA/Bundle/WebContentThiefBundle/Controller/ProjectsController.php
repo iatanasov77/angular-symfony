0c2a3c45452cc2a5e753189e02c7af96e7aeed2c
Ivan Atanasov (office) <iatanasov@icygen.com>
2016-04-05 10:36:18 +0300

    Listing Pages.

--- src/IA/Bundle/WebContentThiefBundle/Controller/ProjectsController.php
+++ src/IA/Bundle/WebContentThiefBundle/Controller/ProjectsController.php
@@ -23,7 +23,7 @@ class ProjectsController extends Controller
             'projects' => $er->findAll(),
             'countProjects' => $er->countTotal()
         );
-        return $this->render('IAWebContentThiefBundle:Projects:list-projects.html.twig', $tplVars);
+        return $this->render('IAWebContentThiefBundle:Projects:list.html.twig', $tplVars);
     }
 
     public function editProjectAction($id)
