a0442398734cf820019bc456deec4cdb45473b7d
Ivan Atanasov (office) <iatanasov@icygen.com>
2016-04-06 12:12:22 +0300

    Менюто вече има приличен за използване завършек.

--- src/IA/Bundle/ApplicationBundle/Menu/Builder.php
+++ src/IA/Bundle/ApplicationBundle/Menu/Builder.php
@@ -17,7 +17,7 @@ class Builder implements ContainerAwareInterface
         $menu = $factory->createItem('root');
 
         //Projects
-        $menu->addChild('Projects', array('uri' => 'javascript:;'));
+        $menu->addChild('Projects', array('uri' => 'javascript:;', 'attributes' => array('iconClass' => 'icon_document_alt')));
         $menu['Projects']->addChild('List Projects', array('route' => 'ia_web_content_thief_list_projects'));
         $menu['Projects']->addChild('Create New Project', array(
             'route' => 'ia_web_content_thief_edit_project',
@@ -25,10 +25,26 @@ class Builder implements ContainerAwareInterface
         ));
 
         // Fieldsets
-        $menu->addChild('Fieldsets', array('uri' => 'javascript:;'));
+        $menu->addChild('Fieldsets', array('uri' => 'javascript:;', 'attributes' => array('iconClass' => 'icon_table')));
         $menu['Fieldsets']->addChild('List Fieldsets', array('route' => 'ia_web_content_thief_fieldsets_list'));
         $menu['Fieldsets']->addChild('Create New Fieldset', array(
             'route' => 'ia_web_content_thief_fieldsets_edit',
+            'routeParameters' => array('id' => 0)
+        ));
+        
+        // Mappers
+        $menu->addChild('Mappers', array('uri' => 'javascript:;', 'attributes' => array('iconClass' => 'icon_documents_alt')));
+        $menu['Mappers']->addChild('List Mappers', array('route' => 'ia_web_content_thief_mappers_list'));
+        $menu['Mappers']->addChild('Create New Mapper', array(
+            'route' => 'ia_web_content_thief_mappers_edit',
+            'routeParameters' => array('id' => 0)
+        ));
+        
+        // Connections
+        $menu->addChild('Connections', array('uri' => 'javascript:;', 'attributes' => array('iconClass' => 'icon_genius')));
+        $menu['Connections']->addChild('List Connections', array('route' => 'ia_web_content_thief_connections_list'));
+        $menu['Connections']->addChild('Create New Connection', array(
+            'route' => 'ia_web_content_thief_connections_edit',
             'routeParameters' => array('id' => 0)
         ));
 
